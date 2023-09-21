2c4c98bc70f01a5df5a9fd53a59e540b23d82332
Zireael <Zireael@whu.edu.cn>
2023-09-21 20:24:34 +0800

    加入relation-graph

--- src/components/RelationG.vue
+++ src/components/RelationG.vue
@@ -1,52 +1,70 @@
 <template>
+  
   <div>
-    <div style="border: #efefef solid 1px; height: calc(100vh - 100px);width: 100%;">
-      <relation-graph ref="relationGraph$" :options="options">
-        <template #node="{node}">
-          <div style="padding-top:20px;">节点：{{node.text}}</div>
-        </template>
-      </relation-graph>
+    <div style="height:calc(100vh - 50px);">
+      <RelationGraph
+        ref="seeksRelationGraph"
+        :options="graphOptions"
+        :on-node-click="onNodeClick"
+        :on-line-click="onLineClick" />
     </div>
+    <el-button type="success" class="c-show-code-button"><el-link href="https://github.com/seeksdream/relation-graph/blob/master/examples/views/seeks-graph-docs/demo/Demo4Logo.vue" target="_blank" style="color: #ffffff;">查看代码</el-link></el-button>
   </div>
 </template>
 
-<script lang="ts">
-import { onMounted, ref } from 'vue'
-import RelationGraph, {RGJsonData} from 'relation-graph/vue3'
-const relationGraph$ = ref<RelationGraph>() as Ref<RelationGraph>
-const options = {
-  defaultExpandHolderPosition: 'right',
-  // defaultLineShape: 4,
-  debug: true,
-  showDebugPanel: true,
-}
-onMounted(() => {
-  const graphJsonData:RGJsonData = {
-    rootId: 'N3',
-    nodes: [
-      { id: 'N4', text: '十4' },
-      { id: 'N5', text: '十5' },
-      { id: 'N6', text: '十6' },
-      { id: 'N7', text: '十7' },
-      { id: 'N3', text: '十三' },
-      { id: 'N9', text: '152****3393' },
-    ],
-    lines: [
-      { from: 'N3', to: 'N9', text: '分享' },
-      { from: 'N3', to: 'N4', text: '分享444' },
-      { from: 'N3', to: 'N5', text: '分享555' },
-      { from: 'N3', to: 'N6', text: '分享666' },
-      { from: 'N3', to: 'N7', text: '分享777' },
-      { from: 'N9', to: 'N4', text: '分享x' }
-    ],
-  };
-  relationGraph$.value.setJsonData(graphJsonData, () => {
-    console.log('relationGraph ready!');
-  })
-})
+<script>
+// 如果您没有在main.js文件中使用Vue.use(RelationGraph); 就需要使用下面这一行代码来引入relation-graph
+import RelationGraph from 'relation-graph/vue3';
 export default {
-  name: 'HelloWorld',
-  components: {
+  name: 'RelationG',
+  components: {RelationGraph},
+  data() {
+    return {
+      isShowCodePanel: false,
+      graphOptions: {
+        debug: true,
+        allowSwitchLineShape: true,
+        allowSwitchJunctionPoint: true,
+        allowShowDownloadButton: true,
+        defaultJunctionPoint: 'border'
+        // 这里可以参考"Graph 图谱"中的参数进行设置
+      }
+    };
   },
-}
+  mounted() {
+    this.showSeeksGraph();
+  },
+  methods: {
+    showSeeksGraph() {
+      const __graph_json_data = {
+        rootId: 'a',
+        nodes: [
+          { id: 'a', text: 'A', borderColor: 'yellow' },
+          { id: 'b', text: 'B', color: '#43a2f1', fontColor: 'yellow' },
+          { id: 'c', text: 'C', nodeShape: 1, width: 80, height: 60 },
+          { id: 'e', text: 'E', nodeShape: 0, width: 150, height: 150 }
+        ],
+        lines: [
+          { from: 'a', to: 'b', text: '关系1', color: '#43a2f1' },
+          { from: 'a', to: 'c', text: '关系2' },
+          { from: 'a', to: 'e', text: '关系3' },
+          { from: 'b', to: 'e', text: '', color: '#67C23A' }
+        ]
+      };
+      this.$refs.seeksRelationGraph.setJsonData(__graph_json_data, (graphInstance) => {
+        // 这些写上当图谱初始化完成后需要执行的代码
+        console.log(graphInstance)
+      });
+    },
+    onNodeClick(nodeObject, $event) {
+      console.log($event)
+      console.log('onNodeClick:', nodeObject);
+    },
+    onLineClick(lineObject, linkObject, $event) {
+      console.log($event)
+      console.log(linkObject)
+      console.log('onLineClick:', lineObject);
+    }
+  }
+};
 </script>
